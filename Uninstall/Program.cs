09f7f407d772ea396ba782084ab0d070eb81c96b
Christian Hermann <c.hermann@bitbeans.de>
2015-09-18 19:35:15 +0200

    Added simple uninstaller

---
+++ Uninstall/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace Uninstall
+{
+    internal class Program
+    {
+        internal static readonly string[] KnownServices =
+        {
+            "dnscrypt-proxy",
+            "dnscrypt-proxy-secondary"
+        };
+
+        private static void Main(string[] args)
+        {
+            UninstallKnownServices();
+        }
+
+        internal static void UninstallKnownServices()
+        {
+            try
+            {
+                foreach (var service in KnownServices)
+                {
+                    var localMachine = Registry.LocalMachine;
+                    var main = localMachine.OpenSubKey(
+                        @"SYSTEM\\CurrentControlSet\\Services\\" + service, false);
+
+                    if (main == null)
+                    {
+                        return;
+                    }
+
+                    var dnsCryptoProxyExecutablePath = (string) main.GetValue("ImagePath");
+                    if (!string.IsNullOrEmpty(dnsCryptoProxyExecutablePath))
+                    {
+                        Console.WriteLine(string.Format("Removing {0}", service));
+                        const int timeout = 9000;
+                        using (var process = new Process())
+                        {
+                            process.StartInfo.FileName = dnsCryptoProxyExecutablePath;
+                            process.StartInfo.Arguments = "--uninstall";
+                            process.StartInfo.UseShellExecute = false;
+                            process.StartInfo.CreateNoWindow = true;
+                            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            process.Start();
+                            if (process.WaitForExit(timeout))
+                            {
+                                if (process.ExitCode == 0)
+                                {
+                                    //do nothing
+                                }
+                            }
+                            else
+                            {
+                                // Timed out.
+                                throw new Exception("Timed out");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ Uninstall/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Uninstall")]
+[assembly: AssemblyDescription("Uninstaller for Simple DNSCrypt")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Uninstall")]
+[assembly: AssemblyCopyright("Copyright © 2015 Christian Hermann")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("70bff12b-996d-43f0-8529-cf8711699acb")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
