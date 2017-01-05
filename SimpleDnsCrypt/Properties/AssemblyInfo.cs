94971f92cd5e3bf2dbc7cc2e7a0f24438ef214fe
Christian Hermann <c.hermann@bitbeans.de>
2017-01-05 09:28:28 +0100

    Prepare 0.4.0

--- SimpleDnsCrypt/Properties/AssemblyInfo.cs
+++ SimpleDnsCrypt/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Windows;
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("0.3.9")]
-[assembly: AssemblyFileVersion("0.3.9")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
