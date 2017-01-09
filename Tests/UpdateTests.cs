e103765a22e2d12824f8b1334d3d25012bad69a4
Christian Hermann <c.hermann@bitbeans.de>
2017-01-09 19:33:58 +0100

    Fix Updater
    - Start adding some tests

---
+++ Tests/UpdateTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using SimpleDnsCrypt.Config;
+using SimpleDnsCrypt.Tools;
+
+namespace Tests
+{
+	[TestFixture]
+    public class UpdateTests
+    {
+		[Test]
+	    public async Task CheckForRemoteUpdateTest()
+	    {
+		    var remoteUpdate = await ApplicationUpdater.CheckForRemoteUpdateAsync();
+			Assert.AreNotEqual(remoteUpdate, null);
+			Assert.AreNotEqual(remoteUpdate.Update, null);
+			Assert.AreNotEqual(remoteUpdate.Update.Installer, null);
+			Assert.AreNotEqual(remoteUpdate.Update.Signature, null);
+			Assert.AreEqual(remoteUpdate.Update.Publickey, Global.ApplicationUpdatePublicKey);
+			Assert.AreEqual(remoteUpdate.Update.Installer.Name, "SimpleDNSCrypt.msi");
+		}
+    }
+}
