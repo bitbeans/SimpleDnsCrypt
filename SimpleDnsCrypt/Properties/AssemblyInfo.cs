aa6fe5a827eea967d7d4164d6130b8fcd67f70f1
Christian Hermann <c.hermann@bitbeans.de>
2017-01-16 18:51:35 +0100

    Prepare 0.4.1
    - Update dnscrypt-proxy to 1.9.2

--- SimpleDnsCrypt/Properties/AssemblyInfo.cs
+++ SimpleDnsCrypt/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Windows;
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("0.4.0")]
-[assembly: AssemblyFileVersion("0.4.0")]
+[assembly: AssemblyVersion("0.4.1")]
+[assembly: AssemblyFileVersion("0.4.1")]
