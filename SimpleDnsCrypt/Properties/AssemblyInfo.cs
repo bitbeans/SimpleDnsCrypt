541932365b40094c6d9f09d88e18383c80a458cc
Christian Hermann <c.hermann@bitbeans.de>
2018-02-11 09:23:28 +0100

    Push version

--- SimpleDnsCrypt/Properties/AssemblyInfo.cs
+++ SimpleDnsCrypt/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Windows;
 	ResourceDictionaryLocation.None,
 	ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("0.4.5")]
-[assembly: AssemblyFileVersion("0.4.5")]
+[assembly: AssemblyVersion("0.4.6")]
+[assembly: AssemblyFileVersion("0.4.6")]
