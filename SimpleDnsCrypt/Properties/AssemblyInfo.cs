94c05d6f9bc7afc812c96dd794ec464830779e65
Christian Hermann <c.hermann@bitbeans.de>
2018-10-04 11:05:36 +0200

    dnscrypt-proxy 2.0.17

--- SimpleDnsCrypt/Properties/AssemblyInfo.cs
+++ SimpleDnsCrypt/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Windows;
 	ResourceDictionaryLocation.None,
 	ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("0.5.8")]
-[assembly: AssemblyFileVersion("0.5.8")]
+[assembly: AssemblyVersion("0.5.9")]
+[assembly: AssemblyFileVersion("0.5.9")]
