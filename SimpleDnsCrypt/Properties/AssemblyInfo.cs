33eeae5874a548e1ff84846089fdc9a119de5984
instant.sc <instant.sc@gmail.com>
2021-03-21 02:07:05 +0700

    automigrate project files using CsprojToVs2017

--- SimpleDnsCrypt/Properties/AssemblyInfo.cs
+++ SimpleDnsCrypt/Properties/AssemblyInfo.cs
@@ -1,18 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
-[assembly: AssemblyTitle("Simple DnsCrypt")]
-[assembly: AssemblyDescription("Simple management tool for dnscrypt-proxy")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Simple DnsCrypt")]
-[assembly: AssemblyCopyright("Copyright © 2015 - 2019 Christian Hermann")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(
 	ResourceDictionaryLocation.None,
 	ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("0.7.1")]
-[assembly: AssemblyFileVersion("0.7.1")]
--- Uninstall/Properties/AssemblyInfo.cs
+++ Uninstall/Properties/AssemblyInfo.cs
@@ -1,15 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Uninstall")]
-[assembly: AssemblyDescription("Uninstaller for Simple DNSCrypt")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Uninstall")]
-[assembly: AssemblyCopyright("Copyright © 2015 - 2019 Christian Hermann")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1a9ca4fe-bdd5-4d7c-a86a-7ed503974718")]
-[assembly: AssemblyVersion("0.2.5")]
-[assembly: AssemblyFileVersion("0.2.5")]
